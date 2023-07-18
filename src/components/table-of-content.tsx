dd9f1ed5465a9670d9277f65c168579249ef848f
Ioofy <mynev.id@gmail.com>
2023-07-18 10:12:04 +0700

    fix: giscuss comments not work

--- src/components/table-of-content.tsx
+++ src/components/table-of-content.tsx
@@ -1,12 +1,43 @@
 import cx from "classnames";
 import {useScroll} from "@hooks/useScroll";
 import {generateTableOfContents} from "@utils/common";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const TableOfContent = ({markdownContents}: {markdownContents: string}) => {
   const isScrolling = useScroll();
   const tableOfContents = generateTableOfContents(markdownContents);
   const [contentSlug, setContentSlug] = useState<string>("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentPosition = window.scrollY;
+      const sections = tableOfContents.map((item) => item.slug);
+
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < sections.length; i++) {
+        const section = document.getElementById(sections[i]);
+
+        if (section) {
+          const sectionTop = section.offsetTop;
+          const sectionBottom = sectionTop + section.offsetHeight;
+
+          if (
+            currentPosition >= sectionTop &&
+            currentPosition < sectionBottom
+          ) {
+            setContentSlug(sections[i]);
+            break;
+          }
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [tableOfContents]);
 
   return (
     <div
@@ -32,10 +63,8 @@ const TableOfContent = ({markdownContents}: {markdownContents: string}) => {
               "no-underline text-gray-400 transition-all duration-300 ease-in-out",
               {
                 "ml-4 text-[15px]": item.level === 3,
-                "ml-6": item.level === 3 && contentSlug === item.slug,
                 "text-[16px]": item.level !== 3,
-                "text-[#3AA6B9] ml-[5px]": contentSlug === item.slug,
-                "border-l-2 px-[7px] border-[#3AA6B9] text-[15px]":
+                "text-[#3AA6B9] ml-[5px] border-l-2 px-[7px] border-[#3AA6B9] text-[15px]":
                   contentSlug === item.slug,
               }
             )}>
