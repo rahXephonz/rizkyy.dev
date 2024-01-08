296106b3c2ae46faf7fbd2aba741897a1a93d66b
Ioofy <mynev.id@gmail.com>
2024-01-08 22:08:47 +0700

    fix: remove twitter profile image on createOgImage

--- src/utils/createOgImage.ts
+++ src/utils/createOgImage.ts
@@ -1,9 +1,8 @@
 // double escape for commas and slashes
-const metaContent = (str: string) =>
-  encodeURIComponent(encodeURIComponent(str));
+const e = (str: string) => encodeURIComponent(encodeURIComponent(str));
 
-export const createOgImage = ({title, meta}: {title: string; meta: string}) => {
-  return [
+export const createOgImage = ({title, meta}: {title: string; meta: string}) =>
+  [
     // ACCOUNT PREFIX
     // Add your own Cloudinary account ID.
     `https://res.cloudinary.com/flawns-site/image/upload`,
@@ -11,25 +10,17 @@ export const createOgImage = ({title, meta}: {title: string; meta: string}) => {
     `w_1600,h_836,q_100`,
 
     // TITLE
-    `l_text:Arial_75:${metaContent(title)},co_rgb:ffff,c_fit,w_1400,h_240`,
+    // Karla google font in light rose
+    `l_text:Arial_80_bold:${e(title)},co_rgb:cfe2f3,c_fit,w_1400,h_240`,
     // Positioning
-    `fl_layer_apply,g_south_west,x_100,y_180`,
+    `fl_layer_apply,g_south_west,x_100,y_350`,
 
-    // OTHER META
-    // Arial, but smaller
-    `l_text:Arial_48:${metaContent(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
+    // META
+    // Karla, but smaller
+    `l_text:Karla_48:${e(meta)},co_rgb:cfe2f380,c_fit,w_1400`,
     // Positioning
-    `fl_layer_apply,g_south_west,x_100,y_100`,
-
-    // PROFILE IMAGE
-    // dynamically fetched from my twitter profile
-    `l_twitter_name:Ioofyy`,
-    // Transformations
-    `c_thumb,g_face,r_max,w_380,h_380,q_100`,
-    // Positioning
-    `fl_layer_apply,w_140,g_north_west,x_100,y_100`,
+    `fl_layer_apply,g_south_west,x_100,y_250`,
 
     // BG
     `bg-gradient_x8l41v.jpg`,
   ].join("/");
-};
